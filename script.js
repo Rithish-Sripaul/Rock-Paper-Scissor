a0e9d5bb3f53242f5d6bec0abff30ecb722901f4
Rithish-Sripaul <rithishsripaul@gmail.com>
2022-09-10 23:03:53 +0530

    Basic Logic of the Game
    The game starts with the initialization of the funciton game() which calls playRound() 5 times while logging the playerScore and computerScore. The maximum amount of score at the end of 5 rounds is the winner.

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 "use strict";
+
+function getComputerChoice() {
+  const choice = ["rock", "paper", "scissor"];
+  const choiceInt = parseInt(Math.random() * 3);
+  return choice[choiceInt];
+}
+
+// Tie: -1
+// Win: 1
+// Lose: 0
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return -1;
+  } else if (
+    (playerSelection == "rock" && computerSelection == "scissor") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissort" && computerSelection == "paper")
+  ) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let result = playRound(getComputerChoice(), getComputerChoice());
+    if (result == 1) {
+      playerScore += 1;
+    } else if (result == 0) {
+      computerScore += 1;
+    }
+  }
+  console.log(playerScore, computerScore);
+  if (playerScore > computerScore) {
+    return true;
+  } else {
+    return false;
+  }
+}
+game();
